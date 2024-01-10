ec5727c9b6043215388365c827684ad7f89d872d
CheCookie <ericwang1156@gmail.com>
2024-01-10 18:09:05 +0800

    nanananananananaaaaaaa

--- assets/js/image_expand.js
+++ assets/js/image_expand.js
@@ -1,11 +1,15 @@
-// 照片 -> 全螢幕顯示
+// Global | 照片 -> 全螢幕顯示
 
+// 讀取元物件 > 啟動全螢幕區塊
+
+// 開啟
 function full_view(ele) {
     let src = ele.getAttribute("src");
     document.querySelector("#img-viewer").querySelector("img").setAttribute("src", src);
     document.querySelector("#img-viewer").style.display = "block";
 }
 
+// 關閉
 function close_model() {
     document.querySelector("#img-viewer").style.display = "none";
 }
